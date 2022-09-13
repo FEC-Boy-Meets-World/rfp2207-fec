de7fb65e3e94b4163a5ee63991449edf5ce54b49
amberlyM <amberm1205@gmail.com>
2022-09-13 15:47:06 -0700

    9-server-setup

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+
+const app = express();
+const port = 3000; //add to .env later
+
+
+
+app.listen(port, () => [
+  console.log(`WW server listening on port ${port}`)
+])

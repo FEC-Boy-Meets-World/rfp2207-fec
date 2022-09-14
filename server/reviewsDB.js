712b8c7552cf6d8e140723bfd2716d3220416695
Thach Do <thachdo90@gmail.com>
2022-09-14 11:18:51 -0700

    Defined schema for reviews, users, and reviewsmeta
    Tested out the collections by inserting one data per collection and checking on Compass

--- server/reviewsDB.js
+++ server/reviewsDB.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 mongoose.connect('mongodb://localhost/reviews')
 
-//define schema
-const reviewsSchema = new mongoose.Schema({
+//define schemas
+const PhotoSchema = new Schema({url: String});
+
+const ReviewSchema = new Schema({
   review_id: {
     type: Number,
     unique: true
@@ -14,15 +17,51 @@ const reviewsSchema = new mongoose.Schema({
   response: String,
   body: String,
   date: Date,
-  reported:
+  reported: Boolean,
   reviewer_name: String,
   helpfulness: Boolean,
-  //change this to an array of strings
-  photos: String
+  photos: {
+    type: [PhotoSchema]
+  }
+})
+
+const ReviewMetaSchema = new Schema({
+  product_id: {
+    type: Number,
+    unique: true
+  },
+  ratings: {
+    1: { type: Number },
+    2: { type: Number },
+    3: { type: Number },
+    4: { type: Number },
+    5: { type: Number }
+  },
+  recommended: Boolean,
+  characteristics: {
+    size:    { type: mongoose.Decimal128 },
+    fit:     { type: mongoose.Decimal128 },
+    comfort: { type: mongoose.Decimal128 },
+    length:  { type: mongoose.Decimal128 },
+    quality: { type: mongoose.Decimal128 },
+    width:   { type: mongoose.Decimal128 }
+  }
+})
+
+const UserSchema = new Schema({
+  user_id: {
+    type: Number,
+    unique: true
+  },
+  email: String,
+  name: String,
 })
 
 //declare class of reviews using schema
-const Reviews = mongoose.model('Reviews', reviewsSchema);
+const Review = mongoose.model('Review', ReviewSchema);
+const ReviewMeta = mongoose.model('ReviewMeta', ReviewMetaSchema);
+const User = mongoose.model('User', UserSchema);
+
 
 
 //define CRUD operations for this class
